4851a682fcc8f9940b2fe473186dd87f74e74043
silvanapenkal <silvanapenkal@gmail.com>
2023-05-19 21:48:40 -0300

    inicialização do projeto

---
+++ src/main/java/tech/devinhouse/aviacao/AviacaoApplication.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.aviacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AviacaoApplication.class, args);
+    }
+
+}
